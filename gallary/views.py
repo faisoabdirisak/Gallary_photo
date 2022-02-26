52f4502cf1fe637874168134f3072f096910878b
faisoabdirisak <faisoabdirisak@gmail.com>
2022-02-26 10:58:15 +0300

    updating the templates and views

--- gallary/views.py
+++ gallary/views.py
@@ -4,8 +4,9 @@ from . models import Category, Photo, Location
 # Create your views here.
 
 def gallary(request):
-    location=Location.objects.all()
-    context={'location':location}
+    categories=Category.objects.all()
+    locations=Location.objects.all()
+    context={'locations':locations, 'categories':categories}
     return render(request,'gallary.html',context)
 
 def viewPhoto(request,pk):
