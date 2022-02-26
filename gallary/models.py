8827c99cab465eff55d6702b63d380e12bc9d8ca
faisoabdirisak <faisoabdirisak@gmail.com>
2022-02-26 09:36:22 +0300

    added location model to the app

--- gallary/models.py
+++ gallary/models.py
@@ -9,7 +9,16 @@ class Category(models.Model):
         return self.name
 
 
+
+class Location(models.Model):
+    name= models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Photo (models.Model):
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL,null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True, blank=True)
     image=models.ImageField(null=False,blank=False,)
     description= models.TextField(max_length=500, null=False, blank=False)
