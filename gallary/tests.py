fd866cb6bee2cad5d7824d9df78d5c501b10f444
faisoabdirisak <faisoabdirisak@gmail.com>
2022-02-26 09:56:06 +0300

    added category test models for test

--- gallary/tests.py
+++ gallary/tests.py
@@ -2,3 +2,12 @@ from django.test import TestCase
 from .models import Category, Photo, Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.travel= Category(name = 'travel')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category))    
