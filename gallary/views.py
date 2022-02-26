be306909ff8314cabf598a2a45d309aa86eb0dfc
faisoabdirisak <faisoabdirisak@gmail.com>
2022-02-26 10:42:29 +0300

    added location model tothe viws

--- gallary/views.py
+++ gallary/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from . models import Category, Photo, Location
 
 # Create your views here.
 
 def gallary(request):
-    return render(request,'gallary.html')
+    location=Location.objects.all()
+    context={'location':location}
+    return render(request,'gallary.html',context)
 
 def viewPhoto(request,pk):
     return render(request,'photo.html')    
