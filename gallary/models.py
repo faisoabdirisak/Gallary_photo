7d919d9b113b855bdbb52856a704fc352a265853
faisoabdirisak <faisoabdirisak@gmail.com>
2022-02-26 09:26:30 +0300

    added photo model to the app

--- gallary/models.py
+++ gallary/models.py
@@ -7,3 +7,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Photo (models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True, blank=True)
+    image=models.ImageField(null=False,blank=False,)
+    description= models.TextField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.description        
