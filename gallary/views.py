bc1153eb695ed637ec83e6598bc9e708801bc940
faisoabdirisak <faisoabdirisak@gmail.com>
2022-02-26 12:04:01 +0300

    added filter methods in locaton

--- gallary/views.py
+++ gallary/views.py
@@ -4,9 +4,14 @@ from . models import Category, Photo, Location
 # Create your views here.
 
 def gallary(request):
+    location =request.GET.get('location')
+    if location==None:
+        photos=Photo.objects.all()
+    else:
+         photos = Photo.objects.filter(location__name = location)
     categories=Category.objects.all()
     locations=Location.objects.all()
-    photos=Photo.objects.all()
+    
     context={'locations':locations, 'categories':categories, 'photos':photos}
     return render(request,'gallary.html',context)
 
