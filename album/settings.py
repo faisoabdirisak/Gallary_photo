d2da02e7c24c87ba35853437213e18faf637787b
faisoabdirisak <faisoabdirisak@gmail.com>
2022-03-01 12:45:34 +0300

    ads

--- album/settings.py
+++ album/settings.py
@@ -100,12 +100,15 @@ WSGI_APPLICATION = 'album.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'album',
-        'USER': 'moringa',
-    'PASSWORD':'faska12',
-    }
+  'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
 }
 
 
